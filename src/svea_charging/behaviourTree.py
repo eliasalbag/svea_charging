a442c483d800a8b0d4b0e4fd6300ec1e2bbd5d9c
eliasalbag <eliasalbag97@gmail.com>
2026-02-10 11:58:10 +0100

    add added behaviour treeskeleton

--- src/svea_charging/behaviourTree.py
+++ src/svea_charging/behaviourTree.py
@@ -4,24 +4,64 @@ from btree import NodeStatus, Node, ActionNode, Sequence, Fallback #imported beh
 
 class btree:
     def __init__(self):
-        #build the behaviour tree here using the imported implementation
-        #the tree should be built according to the our logic, and should use the functions defined below as leaf nodes
-        pass
+        # the tree should be built according to our logic, and should use the functions defined below as leaf nodes
+        self.tree = Sequence(
+            Fallback(
+                ActionNode(self.communicationOk),
+                ActionNode(self.alertCommunicationError),
+                name="communicationGuard",
+            ),
+            Sequence(
+                Fallback(
+                    ActionNode(self.batteryLevelOk),
+                    ActionNode(self.GoToChargingStation),
+                    name="batteryLevelCheck",
+                ),
+                Fallback(
+                    ActionNode(self.vehicleCloseEnoughToCharger),
+                    ActionNode(self.FineAlign),
+                    name="approachCharger",
+                ),
+                Fallback(
+                    ActionNode(self.vehicleDocked),
+                    ActionNode(self.dockVehicle),
+                    name="docking",
+                ),
+                Fallback(
+                    ActionNode(self.chargingProcessOk),
+                    ActionNode(self.alertChargingError),
+                    name="chargingProcessCheck",
+                ),
+                Sequence(
+                    ActionNode(self.doneCharging),
+                    ActionNode(self.undockVehicle),
+                    name="undocking",
+                ),
+            ),
+        )
+
 
     #condition
     def communicationOk(self):
         #check if communication with the car is ok
-        pass
+        if True: #PLACEHOLDER: replace with actual check
+            return NodeStatus.SUCCESS
+        else:
+            return NodeStatus.FAILURE
+        
 
     #action
     def alertCommunicationError(self):
-        #alert the user that communication with the car is not ok
+        #alert the user that communication with the car is not ok, check with Kaj what is the best way to do handle this
         pass
 
     #condition
     def batteryLevelOk(self):
         #check if the battery level is above a charging threshold
-        pass
+        if True: #PLACEHOLDER: replace with actual check
+            return NodeStatus.SUCCESS
+        else:
+            return NodeStatus.FAILURE
 
     #action
     def GoToChargingStation(self):
@@ -31,7 +71,10 @@ class btree:
     #condition
     def vehicleCloseEnoughToCharger(self):
         #check if the vehicle is within some decided proximity of the charger
-        pass
+        if True: #PLACEHOLDER: replace with actual check
+            return NodeStatus.SUCCESS
+        else:
+            return NodeStatus.FAILURE
 
     #action
     def FineAlign(self):
@@ -41,7 +84,10 @@ class btree:
     #condition
     def vehicleDocked(self):
         #check if the vehicle is docked and have contact with the charger
-        pass
+        if True: #PLACEHOLDER: replace with actual check
+            return NodeStatus.SUCCESS
+        else:
+            return NodeStatus.FAILURE
 
     #action
     def dockVehicle(self):
@@ -51,7 +97,10 @@ class btree:
     #condition
     def chargingProcessOk(self):
         #check if the charging process is going ok
-        pass
+        if True: #PLACEHOLDER: replace with actual check
+            return NodeStatus.SUCCESS
+        else:
+            return NodeStatus.FAILURE
 
     #action
     def alertChargingError(self):
@@ -61,7 +110,10 @@ class btree:
     #condition
     def doneCharging(self):
         #check if the battery is sufficiently charged
-        pass
+        if True: #PLACEHOLDER: replace with actual check
+            return NodeStatus.SUCCESS
+        else:
+            return NodeStatus.FAILURE
 
     def undockVehicle(self):
         #some action that would undock the vehicle
