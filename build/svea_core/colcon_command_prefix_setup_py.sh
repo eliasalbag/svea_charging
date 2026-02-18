28aae3b91f0f5cf1ad490024594c20a8769a113b
eliasalbag <eliasalbag97@gmail.com>
2026-02-18 15:55:26 +0100

    add pythonRobotics necessary code

---
+++ build/svea_core/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/eliasalbag/KTH/sveaMasterThesis/svea_charging/install/svea_localization/share/svea_localization/package.sh"
---
+++ build/svea_examples/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/eliasalbag/KTH/sveaMasterThesis/svea_charging/install/svea_localization/share/svea_localization/package.sh"
+. "/home/eliasalbag/KTH/sveaMasterThesis/svea_charging/install/svea_core/share/svea_core/package.sh"
